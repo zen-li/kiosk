2368b233fef873af643ea9e8b5ea083e9a31d164
Galen Dai <galen@leanwing.com>
2017-10-19 11:10:33 +0800

    Fix: Change dianping's domain to https

--- pages/tuangou/index.vue
+++ pages/tuangou/index.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container fluid height="100%">
-        <iframe src="http://t.dianping.com/beijing" frameborder="0" height="1000px" width="100%"></iframe>
+        <iframe src="https://t.dianping.com/beijing" frameborder="0" height="1000px" width="100%"></iframe>
     </v-container>
 </template>
 

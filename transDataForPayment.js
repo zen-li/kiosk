f4e7fb220774d75d9a37186abbbadef2ec34da58
Zen Li <thream@outlook.com>
2017-10-23 17:12:34 +0800

    Update: 修改移动支付页面商品数据的生成

--- transDataForPayment.js
+++ transDataForPayment.js
@@ -1,7 +1,19 @@
 
 const fs = require('fs')
-const data = require('./data')
+
+const books = require('./data/book.json')
+const magazines = require('./data/magazine.json')
+const merchandises = require('./data/merchandise.json')
+const comments = require('./data/comments.json')
+const bookpromo = require('./data/bookpromo.json')
+
+const data = {
+    books: books,
+    magazines: magazines,
+    merchandises: merchandises,
+    comments: comments,
+    bookpromo: bookpromo
+}
 
 const string = 'var resource = ' + JSON.stringify(data)
 fs.writeFileSync('./dist/payment/resource.js', string)
-
